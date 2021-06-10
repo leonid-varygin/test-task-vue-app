cf466d74fe177c743b0981701f8243c722d6ee1c
leonid-varygin <39562139+leonid-varygin@users.noreply.github.com>
2021-06-10 20:39:02 +0700

    cleaning files

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import store from './store'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueTreeList from 'vue-tree-list'
 
-Vue.use(VueTreeList)
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
